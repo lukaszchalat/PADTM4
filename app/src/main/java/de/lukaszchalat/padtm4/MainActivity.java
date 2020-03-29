fc0d6d45494cfab54bee3ec9544f4472be60affb
Lukasz <kontakt@lukaszchalat.de>
2020-03-29 21:01:31 +0200

    DodajWpis added

--- app/src/main/java/de/lukaszchalat/padtm4/MainActivity.java
+++ app/src/main/java/de/lukaszchalat/padtm4/MainActivity.java
@@ -2,7 +2,11 @@ package de.lukaszchalat.padtm4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -27,5 +31,18 @@ public class MainActivity extends AppCompatActivity {
 
         ListView listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(this.adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+
+        return true;
+    }
+
+    public void nowyWpis(MenuItem mi) {
+        Intent intencja = new Intent(this, DodajWpis.class);
+        startActivityForResult(intencja, 1);
     }
 }
